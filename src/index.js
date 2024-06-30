30b671fec498df21ea37d4fa40fb4ad0f68ad2fa
Guilherme Niederauer <guilhermenied01@gmail.com>
2024-06-29 21:24:32 -0300

    Add verification in file extension

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import fs from 'node:fs/promises'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 const imgDirPath = path.join(fileURLToPath(import.meta.url), '../../images')
-function getImagesFiles(path) {
-  const files = fs.readdir(path)
+async function getImagesFiles(filePath) {
+  const files = await fs.readdir(filePath)
+  files.map((file) => {
+    const fileExtension = path.extname(file).split('.')[1]
+    if (fileExtension !== 'jpg' && fileExtension!== 'png' && fileExtension!== 'jpeg') {
+      throw new Error('Invalid image file extension')
+    }
+  })
+  
   return files
 }
 
 getImagesFiles(imgDirPath).then(data => {
-  console.log(data)
+  console.log(data) 
 })
